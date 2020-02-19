9cbe91852beef60800bdbfe3526d867c5caecf0c
Mikita Melnikau <usesa@yandex.com>
2020-02-20 00:55:14 +0300

    scripts in package.json switched

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/todo-list'));
+app.use(express.static('./dist/todo-list/'));
 
 app.get('/*', function(req,res) {
     
